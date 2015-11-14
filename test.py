6d52d645e58586edc57b396db342975111d5cb18
bloesum <ewout.boelsums@gmail.com>
2015-11-14 13:32:25 +0000

    chapter 1

---
+++ test.py
@@ -0,0 +1,10 @@
+#! python3
+import ecommerce.products
+product = ecommerce.products.Product()
+
+from ecommerce import products.Product
+product = Product()
+
+from ecommerce import products
+product = products.Product()
+
